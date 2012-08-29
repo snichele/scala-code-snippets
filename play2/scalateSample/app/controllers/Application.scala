e7179eaf9d04daba6f7acd49c70694aec05c5861
snichele <nichele.sebastien@gmail.com>
2012-08-29 08:40:15 +0200

    Minimal working sample, based on raible + http://janhelwich.wordpress.com/ posts.

--- play2/scalateSample/app/controllers/Application.scala
+++ play2/scalateSample/app/controllers/Application.scala
@@ -6,7 +6,9 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  	import models._
+
+    Ok( Scalate("index.jade").render('user -> User("Raible"))).as(HTML)
   }
   
 }
