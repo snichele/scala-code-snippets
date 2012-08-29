0e205582425fb0e5af5c8185e4f84cd8a7463b99
snichele <nichele.sebastien@gmail.com>
2012-08-29 08:08:57 +0200

    Play 2.0.3 scalate sample initial commit

---
+++ play2/scalateSample/app/controllers/Scalate.scala
@@ -0,0 +1,11 @@
+package controllers
+ 
+import play.mvc.Http
+ 
+trait Scalate {
+ 
+  def render(args: (Symbol, Any)*) = {
+    def defaultTemplate = Http.Request.current().action.replace(".", "/")
+    ScalateTemplate(defaultTemplate).render(args: _*);
+  }
+}
