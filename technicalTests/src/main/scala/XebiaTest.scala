730c8b2feb082d37b412851c47c47a9ac35a894b
snichele <nichele.sebastien@gmail.com>
2012-09-14 10:06:23 +0200

    Custom defined 'types' changed to case classes. OO solution added (unfinished).
    Case classes increase comprehension of the code when reading it : a Position2D is
    clearer than a Tuple2(Int,Int). The drawback is that some parts of the
    code is a little bit more verbose.
    
    Added a more 'Object Oriented' solution (unfinished)

--- technicalTests/src/main/scala/XebiaTest.scala
+++ technicalTests/src/main/scala/XebiaTest.scala
@@ -1,5 +1,21 @@
 
 object XebiaTest {
+    
+  sealed abstract trait Direction
+  case object N extends Direction
+  case object S extends Direction
+  case object E extends Direction
+  case object W extends Direction
+  
+  case class Position2D(x: Int, y: Int)
+  case class MowerState(pos: Position2D, direction: Direction)
+  
+ 
+  val acceptedCommands = List('A, 'G, 'D)
+  def changeDirectionFor(mowerState: MowerState)(withRule: Direction => Direction) : MowerState = MowerState(mowerState.pos,withRule(mowerState.direction))
+  val ┌ : MowerState => MowerState = changeDirectionFor(_)(_ match {case N => E case E => S case S => W case W => N})
+  val ┐ : MowerState => MowerState = changeDirectionFor(_)(_ match {case N => W case E => N case S => E case W => S})
+  
   /*
    Usage :
    launch sbt in the directory of the project
@@ -11,40 +27,26 @@ object XebiaTest {
   object FunctionalSolution {
     // todo add implicit conversion String => Seq[Symbol]
  
-    type Pos2D = Tuple2[Int,Int]
-    type MowerState = Tuple2[Pos2D,Direction]
-    
-    sealed abstract trait Direction
-    case object N extends Direction
-    case object S extends Direction
-    case object E extends Direction
-    case object W extends Direction
-    
-    val acceptedCommands = List('A, 'G, 'D)
-    
     /** Main function to execute. */
-    def runMowerFleet(terrainSize: Pos2D, fleet: Seq[Tuple2[MowerState,String]]) = {
-      val possibleTerrainPositions = for (x <- (0 to terrainSize._1 ); y <- (0 to terrainSize._2 )) yield (x, y) // cartesian product
+    def runMowerFleet(terrainSize: Position2D, fleet: Seq[Tuple2[MowerState,String]]) = {
+      val garden = for (x <- (0 to terrainSize.x ); y <- (0 to terrainSize.y )) yield Position2D(x, y) // cartesian product, pairs a wrapped in a Position2D
 
-      
-      def maybeMoveXOf(mowerState: MowerState)(by: Int) = ( possibleTerrainPositions.find(_.equals((mowerState._1._1 + by, mowerState._1._2))).getOrElse(mowerState._1) ,mowerState._2)
-      def maybeMoveYOf(mowerState: MowerState)(by: Int) = ( possibleTerrainPositions.find(_.equals((mowerState._1._1, mowerState._1._2 + by))).getOrElse(mowerState._1) ,mowerState._2)
+      // todo parametrize garden !
+      def maybeMoveXOf(mowerState: MowerState)(by: Int) = MowerState( garden.find(_.equals(Position2D(mowerState.pos.x + by, mowerState.pos.y))).getOrElse(mowerState.pos) ,mowerState.direction)
+      def maybeMoveYOf(mowerState: MowerState)(by: Int) = MowerState( garden.find(_.equals(Position2D(mowerState.pos.x, mowerState.pos.y + by))).getOrElse(mowerState.pos) ,mowerState.direction)
       val ► = (mowerState: MowerState) => maybeMoveXOf(mowerState)(1)
       val ◄ = (mowerState: MowerState) => maybeMoveXOf(mowerState)(-1)
       val ▲ = (mowerState: MowerState) => maybeMoveYOf(mowerState)(1)
       val ▼ = (mowerState: MowerState) => maybeMoveYOf(mowerState)(-1)
     
-      def changeDirectionFor(mowerState: MowerState)(withRule: Direction => Direction) = (mowerState._1,withRule(mowerState._2))
-      val ┌ = (mowerState: MowerState) => changeDirectionFor(mowerState)((fromDirection: Direction) => fromDirection match {case N => E case E => S case S => W case W => N})
-      val ┐ = (mowerState: MowerState) => changeDirectionFor(mowerState)((fromDirection: Direction) =>  fromDirection match {case N => W case E => N case S => E case W => S})
     
       def runCommands(startMowerState: MowerState)(commands: Seq[Symbol]) = {
-        def nextState(fromState: MowerState, withCommand: Symbol) = {
-          val newState = withCommand match {
+        def nextState(fromState: MowerState, withCommand: Symbol):MowerState = {
+          val newState : MowerState = withCommand match {
             case 'G => ┐(fromState)
             case 'D => ┌(fromState)
             case 'A => {
-                fromState._2 match {
+                fromState.direction match {
                   case N => ▲(fromState)
                   case E => ►(fromState)
                   case S => ▼(fromState)
@@ -66,9 +68,33 @@ object XebiaTest {
               
       val allPositions = for {
         nextMower <- fleet
-        if possibleTerrainPositions.find(_.equals(nextMower._1._1)).isDefined
+        if garden.find(_.equals(nextMower._1.pos)).isDefined
       } yield runCommands(nextMower._1)(nextMower._2.map(c => Symbol(c.toString)))
       allPositions
     }    
+  }
+  
+  object ObjectSolution {
+    case class Garden(width: Int, height: Int)
+    case class Mower(x: Int, y: Int, direction: Direction, garden: Garden) {
+      def moveByCommand(command: Symbol) {
+//        command match {
+//          case 'G => ┐(fromState)
+//          case 'D => ┌(fromState)
+//          case 'A => {
+//              fromState._2 match {
+//                case N => ▲(fromState)
+//                case E => ►(fromState)
+//                case S => ▼(fromState)
+//                case W => ◄(fromState)
+//              }
+//            }
+//          case x: Symbol => {
+//              println(" Unknown command ["+x+"], skipping it.")
+//              identity(fromState)
+//            }        
+//        }
+      }
+    }
   }
 }
--- technicalTests/src/test/scala/XebiaTestSpec.scala
+++ technicalTests/src/test/scala/XebiaTestSpec.scala
@@ -3,6 +3,7 @@ import org.specs2.mutable._
 
 class XebiaTestSpec extends Specification {
        
+  import XebiaTest._
   import XebiaTest.FunctionalSolution._
   
   "XebiaTest.FunctionalSolution  " should {
@@ -10,35 +11,35 @@ class XebiaTestSpec extends Specification {
     " compute a mover starting from 1,2,N to  1,3,N and one from ( (3,3), E) to ((5,1),E)" in {
       
       runMowerFleet(
-        (5,5),
+        Position2D(5,5),
         Seq(
-          (( (1,2), N),"GAGAGAGAA"),
-          (( (3,3), E),"AADAADADDA")
+          (MowerState( Position2D(1,2), N),"GAGAGAGAA"),
+          (MowerState( Position2D(3,3), E),"AADAADADDA")
         )
       ) must containAllOf(
-        List(((1,3),N), ((5,1),E))
+        List(MowerState(Position2D(1,3),N), MowerState(Position2D(5,1),E))
       )
     }
     
     " not permit mower to get out of the field " in {
       runMowerFleet(
-        (5,5),
+        Position2D(5,5),
         Seq(
-          (( (1,2), N),"AAAAAAAAAAAAAAADAAAAAAA")
+          (MowerState( Position2D(1,2), N),"AAAAAAAAAAAAAAADAAAAAAA")
         )
       ) must containAllOf(
-        List(((5,5),E))
+        List(MowerState(Position2D(5,5),E))
       )
     }
     
     " should gobble invalid commands " in {
       runMowerFleet(
-        (5,5),
+        Position2D(5,5),
         Seq(
-          (( (1,2), N),"FOOBIR")
+          (MowerState( Position2D(1,2), N),"FOOBIR")
         )
       ) must containAllOf(
-        List(((1,2),N))
+        List(MowerState(Position2D(1,2),N))
       )
     }
   }
